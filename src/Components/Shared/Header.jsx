0f89537465820acd8f67e73970fa262b69456cc3
ShayanJamil519 <shayanjamil500@gmail.com>
2023-10-12 12:37:45 +0500

    Logo Changed

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -8,7 +8,19 @@ const Header = () => {
       <div className="flex justify-between items-center  py-5 w-[90%] mx-auto ">
         {/* Left */}
         <div className="flex justify-between gap-20 items-center">
-          <img src="/assets/logo.png" alt="logo" />
+          <div className="flex justify-center items-center gap-3">
+            <img
+              src="/assets/logo.png"
+              alt="logo"
+              className="w-[60px] object-cover"
+            />
+            <div>
+              <h1 className="text-[#fff] text-[13px] leading-4 mb-[1px] font-semibold">
+                Greenwashing <br /> Identifier
+              </h1>
+              <p className="text-[#dfdddd] text-[11px]">By ImpactScope</p>
+            </div>
+          </div>
         </div>
 
         <div className="flex justify-center items-center gap-10">
